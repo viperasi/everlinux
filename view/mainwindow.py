be28f160c3e54b4e72ffff5e49fd264fac2d73a2
Arthur.Tsu <xumin@ktrd.com>
2013-01-20 11:54:38 +0800

    add mainwindow icon and main method

--- view/mainwindow.py
+++ view/mainwindow.py
@@ -14,4 +14,15 @@ class MainWindow(QtGui.QMainWindow):
 		self.initUI()
 
 	def initUI(self):
-		
+		self.resize(800,600)
+		self.setWindowTitle('EverLinux -- Evernote client for linux')
+
+		icon = QtGui.QIcon('../icons/enlogo.png')
+		self.setWindowIcon(icon)
+		print 'aaa'
+
+if __name__ == '__main__':
+	app = QtGui.QApplication(sys.argv)
+	main = MainWindow()
+	main.show()
+	sys.exit(app.exec_())
